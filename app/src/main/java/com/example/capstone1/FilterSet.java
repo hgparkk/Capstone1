aff84a59dc8bf8257a4a058a73bb666f9c35b285
phg2559 <phg2559@naver.com>
2024-04-04 12:22:45 +0900

    new commit

---
+++ app/src/main/java/com/example/capstone1/FilterSet.java
@@ -0,0 +1,27 @@
+package com.example.capstone1;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class FilterSet extends BottomSheetDialogFragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        view = inflater.inflate(R.layout.filter_set,container,false);
+
+        view.findViewById(R.id.setFilter).setOnClickListener(v -> dismiss());
+
+        return view;
+    }
+}
