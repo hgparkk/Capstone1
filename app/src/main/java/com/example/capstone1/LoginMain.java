9b19e8c4ec52e76980ec126f6ec2e04dc9febb8a
phg2559 <phg2559@naver.com>
2024-04-29 10:07:57 +0900

    new commit

--- app/src/main/java/com/example/capstone1/LoginMain.java
+++ app/src/main/java/com/example/capstone1/LoginMain.java
@@ -1,6 +1,8 @@
 package com.example.capstone1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,15 @@ public class LoginMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
+
+        //등장 애니메이션
+        overridePendingTransition(R.anim.from_bottom_enter,R.anim.none);
+
+        //Button 변수에 할당하기
+        Button sign = findViewById(R.id.sign);
+        Button findIDPW = findViewById(R.id.findIDPW);
+
+
+
     }
 }
--- app/src/main/java/com/example/capstone1/MyInfo.java
+++ app/src/main/java/com/example/capstone1/MyInfo.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.OnBackPressedDispatcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyInfo extends AppCompatActivity {
@@ -11,6 +13,22 @@ public class MyInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_info);
+
+        //등장 애니메이션
+        overridePendingTransition(R.anim.from_right_enter,R.anim.none);
+
+        OnBackPressedDispatcher onBackPressedDispatcher = getOnBackPressedDispatcher();
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                finish();
+                if(isFinishing()){
+                    overridePendingTransition(R.anim.none,R.anim.to_right_exit);
+                }
+            }
+        };
+        onBackPressedDispatcher.addCallback(this, callback);
 
         //Button 변수에 할당하기
         Button backButton = findViewById(R.id.backButton);
@@ -21,7 +39,12 @@ public class MyInfo extends AppCompatActivity {
         Intent loginMainIntent = new Intent(getApplicationContext(), LoginMain.class);
 
         //뒤로가기 버튼
-        backButton.setOnClickListener(v -> finish());
+        backButton.setOnClickListener(v -> {
+            finish();
+            if(isFinishing()){
+                overridePendingTransition(R.anim.none,R.anim.to_right_exit);
+            }
+        });
 
         //프로필사진 버튼
         loginAccountButton.setOnClickListener(v -> startActivity(loginMainIntent));
